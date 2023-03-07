7146faa55ee3d0276a29bd86d52657e53bd266da
bianca <alexiabianca2002@gmail.com>
2023-03-07 13:25:14 +0200

    lab3

---
+++ src/Printing.hs
@@ -0,0 +1,11 @@
+module Printing (showExp) where
+
+import Exp
+import Data.List (intercalate)
+
+showVar :: Var -> String
+showVar = undefined
+
+showExp :: ComplexExp -> String
+showExp = undefined
+
