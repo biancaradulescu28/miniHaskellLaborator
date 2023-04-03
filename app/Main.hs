b17fc6640a4c8092337dfab60b0d89d5fbb94bbd
bianca <alexiabianca2002@gmail.com>
2023-04-03 15:44:20 +0300

    lab4 end

--- app/Main.hs
+++ app/Main.hs
@@ -3,22 +3,16 @@ module Main where
 import System.IO
 import System.Console.Isocline
 
+import Eval
+import Sugar
 import Exp
 import Parsing
 import Printing
 import REPLCommand
 
 main :: IO ()
-main = do
-    setHistory "history.txt" 200
-    input <- readline "miniHaskell"
-    case parse replCommand "<input>" input of
-      Left err -> print err >> main
-      Right cmd -> case cmd of
-                    Quit -> return ()
-                    Load s -> putStrLn "notImplemented" >> main
-                    Eval l -> case parse exprParser "<input>" l of
-                        Left err -> print err >> main
-                        Right c -> putStrLn ( showExp c ) >> main
-
-
+main = do 
+  Load s -> putStrLn ("notImplemented") >> main
+                        Eval l -> case parse exprParser "<input>" l of
+                            Left err -> print err >> main
+                            Right c -> (putStrLn . showExp . sugarExp . normalize . desugarExp $ c) >> main
